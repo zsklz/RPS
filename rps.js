05bc0dc500a1bbede02fd7f7aed7b96bc6abf592
Zsolt Klauzer <99751489+zsklz@users.noreply.github.com>
2023-07-26 11:29:44 +0200

    Add getComputerChoice()

--- rps.js
+++ rps.js
@@ -1,1 +1,11 @@
 console.log("Hello World!");
+
+// returns computer's choice based on a pseudorandom number
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0)
+	return "rock";
+    else if (choice === 1)
+	return "paper";
+    else return "scissors";
+}
