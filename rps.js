9323763371429fb9c037e3d9d9ff654b494acdd6
Zsolt Klauzer <99751489+zsklz@users.noreply.github.com>
2023-07-26 12:07:17 +0200

    Add playRound()

--- rps.js
+++ rps.js
@@ -1,5 +1,3 @@
-console.log("Hello World!");
-
 // returns computer's choice based on a pseudorandom number
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -9,3 +7,19 @@ function getComputerChoice() {
 	return "paper";
     else return "scissors";
 }
+
+function playRound(playerSelection, computerSelection) {
+    // so we can safely compare
+    playerSelection.toLowerCase();
+    // we check if the strings match
+    if (playerSelection === computerSelection)
+	return "It's a tie."
+    // we check if the player won
+    else if (playerSelection === "rock" && computerSelection === "scissors"
+	     || playerSelection === "paper" && computerSelection === "rock"
+	     || playerSelection === "scissors" && computerSelection === "paper") {
+	return `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+    else return `You lose! ${computerSelection} beats ${playerSelection}.`;
+}
+
